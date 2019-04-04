03ef04a41d39696dba420555ce6ee6b8df7aa0ee
mmilet02 <48136966+mmilet02@users.noreply.github.com>
2019-04-04 17:03:17 +0200

    ...

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div className="navbar" />;
+  }
+}
+
+export default Navbar;
