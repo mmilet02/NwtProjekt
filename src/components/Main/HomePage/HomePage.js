625be41996c3e28c92bb55dc801f48083031d004
mmilet02 <48136966+mmilet02@users.noreply.github.com>
2019-04-06 23:50:32 +0200

    prototip tripList i HomePage 6/4/2019

--- src/components/Main/HomePage/HomePage.js
+++ src/components/Main/HomePage/HomePage.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import "./HomePage.css"
 import home_img from "../../../images/header_background.jpg";
 import transfer from "../../../images/transfer.jpg";
-import hotel from "../../../images/hotel.png";
-import fun from "../../../images/fun.png";
 
 export class HomePage extends Component {
   constructor() {
@@ -30,14 +28,23 @@ export class HomePage extends Component {
     //   );
     // });
     return (
-      <div>
+      <div className="homepage_container">
         <div className="header_container"><img src={home_img} alt="" /></div>
-        <h1>WELCOME</h1>
-        <div className="img_container">
-          <div className="pic"><img src={transfer} alt="" /></div>
-          <div className="pic"><img src={hotel} alt="" /></div>
-          <div className="pic"><img src={fun} alt="" /></div>
+        <div>
+          <div className="heading"> <h1>WELCOME / U PONUDI</h1></div>
+          <div className="img_container">
+            <div className="pic"><img src={transfer} alt="" /></div>
+            <div className="pic"><img src={transfer} alt="" /></div>
+            <div className="pic"><img src={transfer} alt="" /></div>
+            <div className="pic"><img src={transfer} alt="" /></div>
+            <div className="pic"><img src={transfer} alt="" /></div>
+            <div className="pic"><img src={transfer} alt="" /></div>
+            <div className="pic"><img src={transfer} alt="" /></div>
+          </div>
         </div>
+        <div className="about_me"><h1>ABOUT</h1></div>
+        <div className="info"><h1>CONTACT</h1></div>
+
       </div>
     );
   }
--- src/components/Main/TripList/TripList.js
+++ src/components/Main/TripList/TripList.js
@@ -1,10 +1,41 @@
 import React, { Component } from "react";
+import "./TripList.css"
 
 export class TripList extends Component {
   render() {
     return (
       <div>
-        <p>Trip List</p>
+        <div className="filter"><h1>FILTER</h1></div>
+        <div className="trips_heading"> <h1>HEADING</h1></div>
+        <div className="trips">
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+          <div className="trip"> <h1>TRIP</h1></div>
+        </div>
+
       </div>
     );
   }
