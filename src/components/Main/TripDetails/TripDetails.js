25e721260386f5eb9bc7724fc2ef07e4dd7f3edb
mmilet02 <48136966+mmilet02@users.noreply.github.com>
2019-05-18 23:00:34 +0200

    mileta ćuna

--- src/components/Main/TripDetails/TripDetails.js
+++ src/components/Main/TripDetails/TripDetails.js
@@ -26,6 +26,38 @@ export class TripDetails extends Component {
             alt=""
           />
         </div>
+        <div className="infoGlavni">
+          <div className="faDiv">
+            <i class="fas fa-map-marker-alt fa-2x" />
+            <p>{this.state.trip.location}</p>
+          </div>
+          <div className="faDiv">
+            <i class="fas fa-tag fa-2x" />
+            <p>{this.state.trip.price} €</p>
+          </div>
+          <div className="faDiv">
+            <i class="far fa-calendar-alt fa-2x" />
+            <p>{this.state.trip.date}</p>
+          </div>
+          <div className="faDiv">
+            <i class="fas fa-hourglass-start fa-2x" />
+            <p>{this.state.trip.duration} days</p>
+          </div>
+          <div className="faDiv">
+            <i class="fas fa-ticket-alt fa-2x" />
+            <p>{this.state.trip.freeSpace}</p>
+          </div>
+          <div className="faDiv">
+            <i class="fas fa-user-tie fa-2x" />
+            <p>{this.state.trip.company}</p>
+          </div>
+        </div>
+        <div className="tripDescription">
+          <p>{this.state.trip.description}</p>
+        </div>
+        <div className="bookNow">
+          <p>BOOK NOW</p>
+        </div>
       </div>
     );
   }
